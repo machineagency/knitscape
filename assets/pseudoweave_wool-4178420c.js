6bca2dc72e00c8c2d9b5b37e11e9fb7e4d69e58f
branchwelder <htwiggsmith@gmail.com>
2023-10-08 15:40:44 -0700

    Updates

---
+++ assets/pseudoweave_wool-4178420c.js
@@ -0,0 +1,1 @@
+const h=59,i=122,e=[{bitmap:{pixels:[2,0,2,0,2,0,2,0,0,0,0,0],width:2,height:6},pos:[0,0],area:[59,122]}],t=["rgba(235,233,187,1)","rgba(46,47,122,1)","rgba(2,92,51,1)"],a={pixels:[0,0,1,1,1,1,0,0,2,2,2,2],width:1,height:12},s={width:59,height:122,repeats:e,yarnPalette:t,yarnSequence:a};export{s as default,i as height,e as repeats,h as width,t as yarnPalette,a as yarnSequence};
